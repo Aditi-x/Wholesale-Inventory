bcfe86371f709d660d45720a71fc1a45040e8edb
Aditi-x <apms885@gmail.com>
2024-11-26 00:57:15 +0530

    Update and rename Database.c to database.c

--- database.c
+++ database.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Database.h"
+#include "database.h"
 
 // Initialize the database (create tables if they don't exist)
 int initialize_database(Database *db, const char *db_name) {
